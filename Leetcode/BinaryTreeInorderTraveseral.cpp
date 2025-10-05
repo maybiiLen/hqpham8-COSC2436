6e40ebfdbd371401305eff8bf2766a40cdba197f
Henry Pham <henrypham6719@gmail.com>
2025-10-05 15:53:10 -0500

    Add iterative inorder traversal for binary tree
    Implements the inorderTraversal method in Solution class using an explicit stack to traverse a binary tree in-order without recursion.

---
+++ Leetcode/BinaryTreeInorderTraveseral.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>result;
+        stack<TreeNode*>stk;
+        TreeNode* curr = root;
+        while(!stk.empty() || curr != nullptr){
+            while(curr != nullptr){
+                stk.push(curr);
+                curr=curr->left;
+            }
+            curr = stk.top();
+            stk.pop();
+            result.push_back(curr->val);
+            curr = curr->right;
+        }
+        return result;
+    }
+};
